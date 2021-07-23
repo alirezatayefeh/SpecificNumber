70a966b2937d2f5af58c1cac1c722155ed88bfd6
admin <admin@alemi-2.local>
2021-07-23 21:08:48 +0430

    fix Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        // Get array size from user
-//        Scanner input = new Scanner(System.in);
-//        System.out.println("please enter size of array");
-//        int size = input.nextInt();
-//
-//        int[] array = new int[size];
-//
-//        // Get array numbers from user and add to array
-//        System.out.println("please enter "+size+" number:");
-//        for (int i=0; i<size; i++){
-//            array[i] = input.nextInt();
-//        }
-//
-//        //Get specific number from user
-//        System.out.println("Please enter your specific number: ");
-//        int spNumber = input.nextInt();
-//
-//        //Create an instance from MergeSort and FindDuplicate class.
-//        MergeSort mergeSort = new MergeSort();
-//        FindSpecificNumber findSpecificNumber = new FindSpecificNumber();
-//
-//        System.out.println("Given Array");
-//        mergeSort.printArray(array);
-//
-//        mergeSort.sort(array, 0, array.length - 1);
-//
-//        System.out.println("\nSorted array");
-//        mergeSort.printArray(array);
-//
-//        findSpecificNumber.findNumbers(array, spNumber);
+        List<Integer> list = Arrays.asList(2,4,6,8,3,6,12,7,21,14,15,9,1,22,16,18);
+
+        //Get specific number from user
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your specific number: ");
+        int spNumber = input.nextInt();
+
+        //Create an instance from FindDuplicate class.
+        FindSpecificNumber findSpecificNumber = new FindSpecificNumber();
+
+        findSpecificNumber.findNumbers(list, spNumber);
     }
 }
